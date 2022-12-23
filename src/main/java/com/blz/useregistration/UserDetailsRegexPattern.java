7971999dc6f9ad85793205162cb2c4782154912e
vikashc332 <vikashc332@gmail.com>
2022-12-23 15:25:37 +0530

    PaswordRule2

--- src/main/java/com/blz/useregistration/UserDetailsRegexPattern.java
+++ src/main/java/com/blz/useregistration/UserDetailsRegexPattern.java
@@ -6,5 +6,6 @@ public class UserDetailsRegexPattern {
     static String EMAIL = "^([a-z0-9]+([-$%&+.]?[0-9a-z]+))[@][a-z0-9]+[.][a-z]{3,}([.][a-z]{2,})?$";
     public static String PHONE_NUMBER = "^([1-9]+[0-9]+)[\\s][0-9]{10}$";
     static String PASSWORD = "^([a-z]){8,20}$";
+    static String PASSWORD1 = "^(?=.*[a-z])(?=.*[A-Z])(?!.+[0-9]).{8,}$";
 
 }
